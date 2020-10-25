80f19d5d25ce42e3cfd4f2873b52cd9fcc17e4e2
mambax7 <mambax7@gmail.com>
2020-10-25 03:38:41 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ xoops_version.php
@@ -0,0 +1,14 @@
+<?php
+
+$modversion['name'] = 'Programme TV';
+$modversion['version'] = 'finale';
+$modversion['description'] = 'Programme TV gratuit avec www.guidetele.com';
+$modversion['credits'] = '--(melkahdor)--<br>( http://www.melkahdor.com/ ) ';
+$modversion['help'] = 'no';
+$modversion['license'] = 'GPL see LICENSE';
+$modversion['official'] = 0;
+$modversion['image'] = 'images/slogo.gif';
+$modversion['dirname'] = 'progtv';
+
+// Menu
+$modversion['hasMain'] = 1;
